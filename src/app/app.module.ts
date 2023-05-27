ca6b1600b6ebbecf3981c7e20b18a68e614b0d56
Agustin Mellado <aguxmellado@gmail.com>
2023-05-27 16:27:42 -0300

    creacion de estructura de carpetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './modules/auth/pages/login/login.component';
+import { RegistroComponent } from './modules/auth/pages/registro/registro.component';
+import { HomePageComponent } from './modules/home/pages/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/modules/ruleta/ruleta.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PreguntasComponent } from './pages/preguntas/preguntas.component';
+
+
+
+@NgModule({
+  declarations: [
+    PreguntasComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class RuletaModule { }
