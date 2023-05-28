bac8bbfa6c9b80499c302a5c0980eb7d9dcca6bf
Agustin Mellado <aguxmellado@gmail.com>
2023-05-28 16:52:54 -0300

    se agrego bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PreguntasComponent } from './modules/ruleta/pages/preguntas/preguntas.component';
 
 //rutas
 const routes: Routes = [
-  
+  {path:'preguntas', component:PreguntasComponent}
 ];
 
 @NgModule({
