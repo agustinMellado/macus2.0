0825573398c837778b59f8329641c94a500c8e46
Agustin Mellado <aguxmellado@gmail.com>
2023-05-28 20:28:49 -0300

    creacion de variables y contructor, aplicacion de interponlacion y modicacion en navbar

--- src/app/modules/ruleta/pages/preguntas/preguntas.component.ts
+++ src/app/modules/ruleta/pages/preguntas/preguntas.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-preguntas',
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./preguntas.component.css']
 })
 export class PreguntasComponent {
+  
+  pregunta: string;
+  resp1: string;
+  resp2: string;
+  resp3: string;
+  constructor(){
+    this.pregunta='¿Qué son los órganos sexuales externos femeninos?';
+    this.resp1=' El pene y los testículos';
+    this.resp2='El clítoris y los labios vaginales ';
+    this.resp3=' La vagina y el útero';
+  }
+  ngOnInit() {
+  }
+ preguntas(){
+
+
+  this.resp1='hola';
+  }
 
 }
