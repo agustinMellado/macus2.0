85c75dc1cbf82d51ae7c136dab7ca84c95484251
ArianaAraneda <ari.araneda@hotmail.com>
2023-06-04 17:10:56 -0300

    importo las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './modules/auth/pages/login/login.component';
+import { RegistroComponent } from './modules/auth/pages/registro/registro.component';
+import { HomePageComponent } from './modules/home/pages/home-page/home-page.component';
 
 //rutas
 const routes: Routes = [
-  
+    {path:'login', component: LoginComponent},
+  {path:'registro', component: RegistroComponent},
+  {path:'home', component: HomePageComponent},
 ];
 
 @NgModule({
