47f6c253446779aad98e9c6b6e6ca8f6db0d88cc
ArianaAraneda <ari.araneda@hotmail.com>
2023-06-04 17:18:19 -0300

    agrego login html, css y ts

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  email: string = "";
+  password: string = "";
 
+  onSubmit() {
+    // Aquí puedes realizar la lógica de autenticación
+    // Por ejemplo, puedes enviar los datos al servidor para verificar las credenciales
+
+    if (this.email === 'usuario' && this.password === 'contraseña') {
+      // Si las credenciales son válidas, puedes redirigir al usuario a otra página
+      // Por ejemplo, puedes utilizar el enrutador de Angular para navegar a una página de inicio
+    } else {
+      // Si las credenciales son inválidas, puedes mostrar un mensaje de error
+    }
+  }
 }
