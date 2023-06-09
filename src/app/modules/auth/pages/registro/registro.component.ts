6426140519cd89355cc8a7ddd6cf652a2f25134e
ArianaAraneda <ari.araneda@hotmail.com>
2023-06-09 07:28:31 -0300

    se le agrego a la imagen, un enlace a las preguntas

--- src/app/modules/auth/pages/registro/registro.component.ts
+++ src/app/modules/auth/pages/registro/registro.component.ts
@@ -43,7 +43,7 @@ formularioRegistrar: FormGroup;
       const {email, password}= this.formularioRegistrar.value;
       //inicio sesion en firebase llamndo al metodo de mi service
       this.auth.signUp(email, password).then((resp) => {
-        alert("Iniciaste Sesión Correctamente"); //(resp) el navegador tira object Object pero toma al usuario cargado en firebase
+        alert("Registro realizado, iniciando sesion.."); //(resp) el navegador tira object Object pero toma al usuario cargado en firebase
         this.router.navigateByUrl('home'); //si los datos comprobados son correctos ahi podes navegar
       }).catch((error) =>{
         alert("Datos Incorrectos, verifique que el usuario exista") //(error) el navegador te escribe que error tiene
